25bc152c4c6eb79213667bee065427eb38f26d91
Jo-HyeRin <sayhyelin12@naver.com>
2022-09-19 12:20:06 +0900

    Project Start

---
+++ src/test/java/site/metacoding/basicController/BasicControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.basicController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
